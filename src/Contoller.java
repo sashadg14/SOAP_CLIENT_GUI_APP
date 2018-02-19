ad582cfc928a23603145e1c5eb99c7ffcc001f2d
sashadg14 <mamayko.1998@mail.ru>
2018-02-19 23:03:16 +0300

    before creating server

--- src/Contoller.java
+++ src/Contoller.java
@@ -1,56 +1,104 @@
-import service.Section;
-
 import javax.swing.*;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 import java.io.*;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class Contoller {
+    ServiseConnector serviseConnector = new ServiseConnector();
+
     public JTree buildTree(){
         DefaultMutableTreeNode root = new DefaultMutableTreeNode("JsReference");
-        for(String string:getSections())
-        root.add(new DefaultMutableTreeNode(string));
+        for (String string :/*serviseConnector.getSectionsArray()*/getSections())
+            root.add(new DefaultMutableTreeNode(string));
+        JTree jTree = new JTree(root);
 
-        JTree jTree=new JTree(root);
         jTree.setShowsRootHandles(true);
         jTree.setRootVisible(false);
         return jTree;
     }
 
-    public List<String> getSections(){
-        List<String> sectionList=new ArrayList<>();
-        File mainDir=new File("JsReference");
-        for(File file: Objects.requireNonNull(mainDir.listFiles()))
-            sectionList.add(file.getName());
+    public List<String> getSections() {
+        List<String> sectionList = new ArrayList<>();
+        /*File mainDir = new File("JsReference");
+        for (File file : Objects.requireNonNull(mainDir.listFiles()))
+            sectionList.add(file.getName());*/
+        try {
+            sectionList= /*new DataBaseConnector().getSections()*/Arrays.asList(serviseConnector.getSectionsArray());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
         return sectionList;
     }
 
-    public List<String> getSubsections(String sectionName){
-        List<String> strings=new ArrayList<>();
-        for (String str:getSections())
-            if(str.equals(sectionName)){
-                File file=new File("JsReference\\"+sectionName);
-                for(File file1:file.listFiles()) {
+    public List<String> getSubsections(String sectionName) {
+        List<String> strings = new ArrayList<>();
+        /*for (String str : getSections())
+            if (str.equals(sectionName)) {
+                File file = new File("JsReference\\" + sectionName);
+                for (File file1 : file.listFiles()) {
                     strings.add(file1.getName());
                 }
-            }
+            }*/
+        try {
+            strings=new DataBaseConnector().getSubections(sectionName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return strings;
     }
 
-    public String getInfoFromSubsection(String sectionName,String subsectionName) throws IOException {
-        File file=new File("JsReference\\"+sectionName+"\\"+subsectionName);
+    public String getInfoFromSubsection(String sectionName, String subsectionName){
+        /*File file = new File("JsReference\\" + sectionName + "\\" + subsectionName);
         FileInputStream fstream = new FileInputStream(file);
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         String strLine;
-        StringBuilder stringBuilder=new StringBuilder();
-        while ((strLine = br.readLine()) != null){
-            stringBuilder.append(strLine+"\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((strLine = br.readLine()) != null) {
+            stringBuilder.append(strLine + "\n");
+        }*/
+        try {
+            return new DataBaseConnector().getSubectionData(sectionName,subsectionName);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return stringBuilder.toString();
+        return "";
     }
+
+    public void deleteSubsection(String sectionName, String subsectionName){
+        try {
+            new DataBaseConnector().deleteSubsection(sectionName,subsectionName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setInfoToSubsection(String sectionName, String subsectionName, String newText) throws IOException {
+        /*File file = new File("JsReference\\" + sectionName + "\\" + subsectionName);
+        PrintWriter printWriter = new PrintWriter(file);
+        printWriter.print(newText);
+        printWriter.close();*/
+        String s=newText.replace("'","\\'");
+       // System.out.println(s);
+        try {
+            new DataBaseConnector().setSubsectionData(sectionName,subsectionName,s);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addSubsection(String sectionName, String subsectionName){
+        try {
+            new DataBaseConnector().addNewSubsection(sectionName,subsectionName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,65 +1,137 @@
-import javafx.scene.layout.Border;
-
 import javax.swing.*;
-import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
-import java.awt.*;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GUI {
-    JFrame jFrame=new JFrame("JS Guide");
-    JButton reloadButton=new JButton(new ImageIcon("src\\resources\\resize.png"));
+    JFrame jFrame = new JFrame("JS Guide");
+    JButton reloadButton = new JButton(new ImageIcon("src\\resources\\resize.png"));
     JTextArea jTextArea;
-    public GUI(){
-       jFrame.getContentPane().setLayout(null);
+    JButton editTextButton;
+    JButton addSubsectionButton;
+    JButton addSectionButton;
+    JButton deleteSubsectionButton;
+    Contoller contoller = new Contoller();
+
+
+    public GUI() {
+        deleteSubsectionButton = new JButton("Удалить подраздел");
+        editTextButton = new JButton("Изменить содержимое");
+        addSubsectionButton = new JButton("Добавить подраздел");
+        addSectionButton= new JButton("Добавить раздел");
+
+        jFrame.getContentPane().setLayout(null);
         jFrame.add(reloadButton);
-        reloadButton.setBounds(0,0,40,30);
+        reloadButton.setBounds(0, 0, 40, 30);
         jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        jTextArea=createTextAreaAtScrollPane();
+        jTextArea = createTextAreaAtScrollPane();
+        reloadButton.addActionListener(e -> {
+                createTree();
+        });
 
-        createTree();
+        addSubsectionButton.setBounds(200, 0, 300, 30);
+        addSubsectionButton.addActionListener(createAddSubsectionBtnActionListener());
+        jFrame.add(addSubsectionButton);
+
+        addSectionButton.setBounds(200, 50, 300, 30);
+        addSectionButton.addActionListener(createAddSectionBtnActionListener());
+        jFrame.add(addSectionButton);
+
+        deleteSubsectionButton.setBounds(200, 100, 300, 30);
+        deleteSubsectionButton.addActionListener(createDeleteBtnActionListener());
+        jFrame.add(deleteSubsectionButton);
+
+        editTextButton.setBounds(1000, 700, 200, 30);
+        editTextButton.addActionListener(createEditBtnActionListener());
+        jFrame.add(editTextButton);
+
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    private void createTree(){
-        JTree jTree=new Contoller().buildTree();
-        JScrollPane jScrollPane=new JScrollPane(jTree);
-        jTree.addTreeSelectionListener(new TreeSelectionListener() {
-            @Override
-            public void valueChanged(TreeSelectionEvent e)
-                {
-                    TreePath parPath=e.getPath().getParentPath();
-                    String selected=jTree.getLastSelectedPathComponent().toString();
-                    DefaultMutableTreeNode node = (DefaultMutableTreeNode)jTree.getLastSelectedPathComponent();
-
-                    if(parPath.toString().equals("[JsReference]")){
-                        for(String s:new Contoller().getSubsections(selected))
-                            node.add(new DefaultMutableTreeNode(s));
-                        jTree.expandPath(e.getPath());
-                    }else {
-                        try {
-                            jTextArea.setText(new Contoller().getInfoFromSubsection(parPath.getLastPathComponent().toString(),selected));
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
-                        }
-                    }
-                }
-        });
-        jScrollPane.setBounds(20,100,200,700);
-        jFrame.getContentPane().add(jScrollPane);
+    private ActionListener createEditBtnActionListener() {
+        return e -> {
+            try {
+                contoller.setInfoToSubsection(parPath.getLastPathComponent().toString(),selectedObj.toString(),jTextArea.getText());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        };
     }
 
-    private JTextArea createTextAreaAtScrollPane(){
-        JTextArea jTextArea=new JTextArea();
-        JScrollPane jScrollPane=new JScrollPane(jTextArea);
-        jScrollPane.setBounds(300,100,1000,500);
+    private ActionListener createDeleteBtnActionListener() {
+        return e -> {
+            if(selectedObj!=null) {
+                contoller.deleteSubsection(parPath.getLastPathComponent().toString(), selectedObj.toString());
+                createTree();
+            }
+        };
+    }
+
+    private ActionListener createAddSubsectionBtnActionListener() {
+        return e -> {
+                String n=JOptionPane.showInputDialog ("Название подраздела");
+                if(n!=null&&!n.equals(""))
+                contoller.addSubsection(selectedSection.toString(),n);
+                createTree();
+        };
+    }
+
+    private ActionListener createAddSectionBtnActionListener() {
+        return e -> {
+                String sectionName=JOptionPane.showInputDialog ("Название раздела");
+                String subSectionName=JOptionPane.showInputDialog ("Название подраздела");
+                if(sectionName!=null&&!sectionName.equals("")&&subSectionName!=null&&!subSectionName.equals(""))
+                contoller.addSubsection(sectionName,subSectionName);
+           createTree();
+        };
+    }
+    JTree jTree;
+    JScrollPane jScrollPane;
+    private void createTree(){
+        if(jScrollPane!=null)
+        jFrame.remove(jScrollPane);
+        jTree= new Contoller().buildTree();
+        jScrollPane = new JScrollPane(jTree);
+        jScrollPane.setBounds(20, 100, 200, 700);
+        jFrame.add(jScrollPane);
+        jScrollPane.updateUI();
+        jTree.addTreeSelectionListener(createTreeSelectionListener(jTree));
+    }
+
+    TreePath parPath;
+    Object selectedObj;
+    Object selectedSection;
+
+    private TreeSelectionListener createTreeSelectionListener(JTree jTree) {
+        return e -> {
+            parPath = e.getPath().getParentPath();
+            selectedObj = jTree.getLastSelectedPathComponent();
+            String selected = selectedObj.toString();
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree.getLastSelectedPathComponent();
+            if (parPath.toString().equals("[JsReference]")) {
+                addSubsectionButton.setText("Добавить подраздел в " + selected);
+                for (String s : new Contoller().getSubsections(selected))
+                    node.add(new DefaultMutableTreeNode(s));
+                jTree.expandPath(e.getPath());
+                selectedObj=null;
+                selectedSection=jTree.getLastSelectedPathComponent();
+            } else {
+                    jTextArea.setText(new Contoller().getInfoFromSubsection(parPath.getLastPathComponent().toString(), selected));
+            }
+        };
+    }
+
+
+
+    private JTextArea createTextAreaAtScrollPane() {
+        JTextArea jTextArea = new JTextArea();
+        JScrollPane jScrollPane = new JScrollPane(jTextArea);
+        jScrollPane.setBounds(300, 150, 1000, 500);
         jFrame.add(jScrollPane);
         return jTextArea;
     }
